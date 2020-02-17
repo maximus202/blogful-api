ffa5b77583840fbf6b8d92584da19f2328db8988
Max R <maxx.ratto@gmail.com>
2020-02-17 14:50:12 -0700

    New commit.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
+const knex = require('knex')
 const app = require('./app')
 
-const { PORT } = require('./config')
+const { PORT, DB_URL } = require('./config')
+
+const database = knex({
+    client: 'pg',
+    connection: DB_URL
+})
+
+app.set('db', database)
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
